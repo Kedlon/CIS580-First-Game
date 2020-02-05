2e232f365f37dfba23e7802fc094d7d185b854f4
Kevin Nolde <kedlon@ksu.edu>
2020-02-05 17:37:52 -0600

    updated to in-class example from instructor from 2/5/20

--- MonoGameWindowsStarter/BoundingCircle.cs
+++ MonoGameWindowsStarter/BoundingCircle.cs
@@ -9,12 +9,24 @@ namespace MonoGameWindowsStarter
 {
     public struct BoundingCircle
     {
+        /// <summary>
+        /// The center X coordinate of the circle
+        /// </summary>
         public float X;
 
+        /// <summary>
+        /// The center Y coordinate of the circle
+        /// </summary>
         public float Y;
 
+        /// <summary>
+        /// The radius of the circle
+        /// </summary>
         public float Radius;
 
+        /// <summary>
+        /// Gets or sets the circle's center vector
+        /// </summary>
         public Vector2 Center {
             get => new Vector2(X, Y);
             set { 
@@ -23,13 +35,18 @@ namespace MonoGameWindowsStarter
             }
         }
 
-        public static implicit operator Rectangle(BoundingCircle cr)
+        /// <summary>
+        /// Casts the circle into a rectangle with tangental sides
+        /// </summary>
+        /// <param name="c"></param>
+        public static implicit operator Rectangle(BoundingCircle c)
         {
             return new Rectangle(
-                (int)(cr.X - cr.Radius),
-                (int)(cr.Y - cr.Radius),
-                (int)(cr.Radius * 2),
-                (int)(cr.Radius * 2));
+                (int)(c.X - c.Radius),
+                (int)(c.Y - c.Radius),
+                (int)(2 * c.Radius),
+                (int)(2 * c.Radius)
+                );
         }
 
     }
